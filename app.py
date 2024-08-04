b98977d2a91ba45678d30d0b1bb719b98fd03996
AhmetNeoSari <asari4137@gmail.com>
2024-08-04 19:27:05 +0300

    add argparse to main function

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-import os
 import time
-import sys
-
 import cv2
-import numpy as np
-import onnxruntime
-import torch
-import threading
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(BASE_DIR)
+import argparse
 
 from face_detection.scrfd.face_detector import Face_Detector
 from face_tracking.tracker.byte_tracker import BYTETracker
 from recognize import Face_Recognize
-
 
 face_detector_dict = {
     "model_file"  : "/home/ahmet/workplace/face_recognition/face_detection/scrfd/weights/scrfd_2.5g_bnkps.onnx",
@@ -51,35 +41,48 @@ face_tracker_dict = {
     "min_box_area": 10,
     "aspect_ratio_thresh": 1.6,
     "ckpt": "bytetrack_s_mot17.pth.tar",
-    "fps" : 30,
     "track_img_size" : (128,128)
 }
 
 
-def main():
-
-    """Main function to start face tracking and recognition threads."""
-
+def main(args):
     cap = cv2.VideoCapture(0)
     face_recognizer = Face_Recognize(**face_recognizer_dict)
     face_detector = Face_Detector(**face_detector_dict)
     face_tracker = BYTETracker(**face_tracker_dict)
+
     frame_id = 0
+    start_time = time.time_ns()
+    frame_count = 0
+    fps = -1
     id_face_mapping = {}
     while True:
         _, frame = cap.read()
         frame_id += 1
 
         outputs, img_info, bboxes, landmarks = face_detector.detect_tracking(frame)
-        tracking_image, data_mapping = face_tracker.track(outputs, img_info, bboxes, landmarks, id_face_mapping,frame_id)
-        id_face_mapping ,caption = face_recognizer.recognize(img_info["raw_img"] ,bboxes, landmarks, data_mapping)
+        tracking_image, data_mapping = face_tracker.track(outputs, img_info, bboxes, fps, landmarks, id_face_mapping, frame_id)
+        id_face_mapping,caption = face_recognizer.recognize(img_info["raw_img"] ,bboxes, landmarks, data_mapping)
 
-        cv2.imshow("frame", tracking_image)
-        # Press 'Q' on the keyboard to exit
-        if cv2.waitKey(25) & 0xFF == ord("q"):
-            break
+        end_timer = time.time()
+        frame_count += 1
+        if frame_count >= 30:
+            fps = 1e9 * frame_count / (time.time_ns() - start_time)
+            frame_count = 0
+            start_time = time.time_ns()
 
+        if args.show:
+            cv2.imshow("frame", tracking_image)
+            # Press 'Q' on the keyboard to exit
+            if cv2.waitKey(25) & 0xFF == ord("q"):
+                break
+                
         
-
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--show', action='store_true', help='Enable showing the result of tracker')
+    parser.add_argument('--no-show', dest='show', action='store_false', help='Disable showing the result of tracker')
+    parser.set_defaults(show=True)
+    args = parser.parse_args()
+
+    main(args)
