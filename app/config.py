7e8b94c9f10a6cabe278de0afb6bf0d974ae459f
AhmetNeoSari <asari4137@gmail.com>
2024-08-08 09:20:04 +0300

    Add comment lines to config.py

--- app/config.py
+++ app/config.py
@@ -3,9 +3,25 @@ import toml
 
 @dataclass
 class Config:
+    """
+    A class to handle loading configuration files for different environments.
+
+    Attributes:
+        config_dir (str): The directory where configuration files are located. Default is "configs".
+    """
     config_dir: str = field(default="configs")
 
     def load(self, env: str):
+        """
+        Load the configuration file for the specified environment.
+
+        Args:
+            env (str): The environment name for which to load the configuration. Examples: "local", "prod".
+        Returns:
+            dict: A dictionary containing the configuration data read from the file.
+        Raises:
+            Exception: If the configuration file for the specified environment is not found, raises an exception.
+        """
         config_path = f"{self.config_dir}/config.{env}.toml"
         try:
             with open(config_path, 'r') as file:
