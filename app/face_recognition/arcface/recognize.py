dd647cc65b4b20324eda0aafa835a5a1a99eb14c
AhmetNeoSari <asari4137@gmail.com>
2024-08-26 11:12:07 +0300

    add tracker algorithm to recognize

--- app/face_recognition/arcface/recognize.py
+++ app/face_recognition/arcface/recognize.py
@@ -4,11 +4,9 @@ import torch
 from torchvision import transforms
 import argparse
 from dataclasses import dataclass
-from typing import Any
-
-if __name__ == "__main__":
-    from recognizer_utils import norm_crop, compare_encodings, read_features, iresnet_inference
-else:
+from typing import Any, Callable
+import time
+if not __name__ == "__main__":
     from .recognizer_utils import norm_crop, compare_encodings, read_features, iresnet_inference
 
 
@@ -37,7 +35,8 @@ class Face_Recognize:
         # Mapping of face IDs to names
         self.id_face_mapping = {}
 
-    def recognize(self, frame ,bboxes, landmarks, data_mapping:dict, is_tracker_available:bool):
+
+    def recognize(self, frame ,face_bboxes, person_bboxes ,landmarks, data_mapping:dict, is_tracker_available:bool):
         """
         Perform face recognition on the provided frame.
 
@@ -52,43 +51,53 @@ class Face_Recognize:
                 - id_face_mapping (dict): Mapping of face IDs to recognition results.
                 - caption (str): Recognition result for the current frame.
         """
-        self.id_face_mapping = {}
-        if is_tracker_available == False:
-            # captions = {"id",name}  # Dictionary to store captions for each bounding box
-            counter = -1
-            for bbox, landmark in zip(bboxes, landmarks):
-                # Face alignment
-                counter += 1
-                face_alignment = norm_crop(img=frame, landmark=landmark)
+        # if is_tracker_available == False:
+        #     self.id_face_mapping = {}
+        # # captions = {"id",name}  # Dictionary to store captions for each bounding box
+        #     counter = -1
+        #     if len(face_bboxes) == 0:
+        #         self.id_face_mapping[0] = "UN_KNOWN"
+        #         return
 
-                score, name = self.recognition(face_image=face_alignment)
+        #     for bbox, landmark in zip(face_bboxes, landmarks):
+        #         counter += 1
+        #         face_alignment = norm_crop(img=frame, landmark=landmark)
 
-                if name is not None:
-                    if score < self.recognition_score_thresh:
-                        caption = "UN_KNOWN"
-                    else:
-                        caption = f"{name}:{score:.2f}"
-                else:
-                    caption = "UN_KNOWN"
+        #         score, name = self.recognition(face_image=face_alignment)
+                
+        #         if name is not None:
+        #             if score < self.recognition_score_thresh:
+        #                 caption = "UN_KNOWN"
+        #             else:
+        #                 caption = f"{name}:{score:.2f}"
+        #         else:
+        #             caption = "UN_KNOWN"
 
-                # Use bbox as key to ensure unique identification
-                self.id_face_mapping[counter] = caption
-            return
+        #         # Use bbox as key to ensure unique identification
+        #         self.id_face_mapping[counter] = caption
+        #     return
         
         if data_mapping["tracking_bboxes"] == None:
             self.logger.error("data_mapping must be provided when is_tracker_use is True")
             raise ValueError("data_mapping must be provided when is_tracker_use is True")
         
         detection_landmarks = landmarks
-        detection_bboxes = bboxes
+        face_detection_bboxes = face_bboxes
         tracking_ids = data_mapping["tracking_ids"]
         tracking_bboxes = data_mapping["tracking_bboxes"]
 
         for i in range(len(tracking_bboxes)):
-            for j in range(len(detection_bboxes)):
+            if tracking_ids[i] in self.id_face_mapping:
+                if self.id_face_mapping[tracking_ids[i]] != "UN_KNOWN":
+                    continue
+                
+            self.id_face_mapping[tracking_ids[i]] = "UN_KNOWN"
+            for j in range(len(face_detection_bboxes)): # complexity O(n) = 1
+
                 mapping_score = self.mapping_bbox(box1=tracking_bboxes[i],
-                                                box2=detection_bboxes[j])
+                                                box2=person_bboxes)
                 if mapping_score > self.mapping_score_thresh:
+
                     face_alignment = norm_crop(img=frame,
                                             landmark=detection_landmarks[j])
 
@@ -97,14 +106,31 @@ class Face_Recognize:
                         if score < self.recognition_score_thresh:
                             caption = "UN_KNOWN"
                         else:
+                            # if name in self.id_face_mapping[tracking_ids[i]]
                             caption = f"{name}:{score:.2f}"
-                    self.id_face_mapping[tracking_ids[i]] = caption
 
-                    detection_bboxes = np.delete(detection_bboxes, j, axis=0)
+                    new_id = tracking_ids[i]
+                    new_value = caption
+
+                    person_name = new_value.split(':')[0]
+                    old_key = None
+                    for key,value in self.id_face_mapping.items():
+                        if person_name in value:
+                            old_key = key
+                            break
+                    
+                    if old_key is not None:
+                        del self.id_face_mapping[old_key]
+                        self.id_face_mapping[new_id] = new_value
+                    else:
+                        self.id_face_mapping[new_id] = new_value
+
+                    # self.id_face_mapping[tracking_ids[i]] = caption
+
+                    face_detection_bboxes = np.delete(face_detection_bboxes, j, axis=0)
                     detection_landmarks = np.delete(detection_landmarks, j, axis=0)
 
                     break
-
         if tracking_bboxes == []:
             caption = "UN_KNOWN"
 
@@ -155,14 +181,21 @@ class Face_Recognize:
                 - score (float): The recognition score.
                 - name (str): The recognized name.
         """
-        # Get feature from face
-        query_emb = self.get_feature(face_image)
+        try:
+            # Get feature from face
+            query_emb = self.get_feature(face_image)
 
-        score, id_min = compare_encodings(query_emb, self.images_embs)
-        name = self.images_names[id_min]
-        score = score[0]
+            score, id_min = compare_encodings(query_emb, self.images_embs)
+            name = self.images_names[id_min]
+            score = score[0]
+            
+            if score < self.recognition_score_thresh:
+                return score, "UN_KNOWN"
 
-        return score, name
+            return score, name
+        except ValueError:
+            # If there are no embeddings in the database, return unknown
+            return 0, "UN_KNOWN"
 
 
     def mapping_bbox(self, box1, box2):
@@ -200,11 +233,10 @@ class Face_Recognize:
 
 
 
-if __name__ == "__main__":
-    """Main function to start recognition threads."""      
-
+if __name__ == "__main__":     
     import os
     import sys
+    from recognizer_utils import norm_crop, compare_encodings, read_features, iresnet_inference
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
     parent_dir = os.path.dirname(current_dir)
@@ -278,6 +310,11 @@ if __name__ == "__main__":
     data_mapping = None
     id_face_mapping = {}
     while True:
+        star_timer = time.time()
         _, frame = cap.read()
-        outputs, bboxes, landmarks = face_detector.detect_tracking(frame)
+        outputs, bboxes, landmarks = face_detector.detect(frame)
         face_recognizer.recognize(frame ,bboxes, landmarks, data_mapping, False)
+        end_time = time.time()
+
+        fps = 1 / (end_time-star_timer)
+        print("fps: ",fps)
